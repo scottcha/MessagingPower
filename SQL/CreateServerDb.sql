14655b01913d049df6e2d3ea2f410bc06efff92a
Scott Chamberlin <scottcha@microsoft.com>
2020-12-14 12:01:23 -0800

    Update sql with create table

--- SQL/CreateServerDb.sql
+++ SQL/CreateServerDb.sql
@@ -113,4 +113,28 @@ GO
 ALTER DATABASE [MessagingDb] SET  READ_WRITE 
 GO
 
+USE [MessagingDb]
+GO
+
+/****** Object:  Table [dbo].[ServerMessages]    Script Date: 12/14/2020 11:59:56 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ServerMessages](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Message] [nvarchar](max) NOT NULL,
+	[SenderId] [nvarchar](50) NOT NULL,
+	[ReceiverId] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_ServerMessages] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+
 
